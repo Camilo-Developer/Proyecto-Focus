67f9e8d59e65f63e45885fd1e5ae48aa6acdcc78
Juan Camilo Developer <juan.camilorodrigez@gmail.com>
2024-02-14 20:29:17 -0500

    Prueba

--- config/app.php
+++ config/app.php
@@ -187,7 +187,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
     ])->toArray(),
 
 ];
